98053a3c5d7d6b15f57674c46e03b98a8af30029
MunerahS <67203726+MunerahS@users.noreply.github.com>
2020-06-20 22:17:31 +0300

    Add files via upload
    https://www.tinkercad.com/things/0eC726v7lDU-start-simulating/editel?lessonid=EHD2303J3YPUS5Z&projectid=OIYJ88OJ3OPN3EA&collectionid=OIYJ88OJ3OPN3EA&tenant=circuits#/lesson-viewer?sharecode=JGPNkh-ibnO2QX7Ts2-U7QBbNWySujAys5O5iwT5XEs

---
+++ start_simulating1.ino
@@ -0,0 +1,18 @@
+void setup()
+{
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT);
+ 
+}
+
+void loop()
+{
+  digitalWrite(6, HIGH);
+  digitalWrite(5, LOW);
+  digitalWrite(4, HIGH);
+  digitalWrite(3, LOW);
+  delay(1000); // 1000ms is equal to 1s
+  
+}
